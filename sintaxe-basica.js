4ab19c8ce81d3143b77e0ce5fc5ccb3c26bb3008
rk <brenotm10@gmail.com>
2022-10-19 10:17:52 -0300

    estudos

---
+++ sintaxe-basica.js
@@ -0,0 +1,46 @@
+//statems terminam com ponto e virgula
+var x=15; 
+var y=12;
+
+// valores: literais e variáveis
+
+//literais
+
+10;
+10.5;
+
+"Olá mundo";
+'Olá mundo';
+
+// variaveis
+
+var x;
+
+// operadores
+
+x = 10;
+
+10 + 5;
+
+20 - 10;
+
+40 * 5;
+
+20 / 10;
+
+
+// expressões
+
+y + 10;
+
+20 * 30;
+
+x * y;
+
+// identificadores 
+
+var $1;
+var _1;
+var ola;
+var olaMundoMeu;
+
---
+++ tipos-de-dados.js
@@ -0,0 +1,33 @@
+// numeros
+
+10;
+10.5;
+
+// strings
+"olá";
+'olá';
+
+// booleanos
+
+true;
+false;
+
+// arrays
+
+[10,"oi",false];
+
+// objetos
+
+{ nome: 'João', sobrenome: 'das Neves' };
+
+// tipagem dinamica
+
+var y = 15;
+y = "olá";
+
+// outros valores
+
+var z; //undefined
+
+var pessoa = null;
+
