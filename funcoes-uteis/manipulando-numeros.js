7928a96a89a7db9737302c44215a6c1acfc3c963
rk <brenotm10@gmail.com>
2022-10-20 07:52:43 -0300

    estudando

---
+++ funcoes-uteis/manipulando-numeros.js
@@ -0,0 +1,13 @@
+var x = 456e5; //456000000
+var y = 123e-6 // 0.000123
+
+var z = 9999999999999999;
+
+var a = 0xFFF;
+
+var b = 07777;
+console.log(b.toString(16));
+
+console.log(x.toExponential());
+
+console.log((3.357).toPrecision(2));
---
+++ funcoes-uteis/manipulando-strings.js
@@ -0,0 +1,2 @@
+var s = "Ned,Jon,Robb,Sansa,Bran,Arya,Rickon";
+console.log(s.split(","));
